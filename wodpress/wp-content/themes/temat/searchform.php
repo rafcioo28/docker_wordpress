1024322db1ee76bcd2219dd0ecdd02e30879fb6c
Rafał Herszkowicz <rafal.herszkowicz@outlook.com>
2020-08-06 10:51:55 +0200

    Init repo docker/wordpress

---
+++ wodpress/wp-content/themes/temat/searchform.php
@@ -0,0 +1,6 @@
+<!-- search -->
+<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
+	<input class="search-input" type="search" name="s" placeholder="Wpisz tekst...">
+	<button class="search-submit" type="submit" role="button">Szukaj</button>
+</form>
+<!-- /search -->
---
+++ wodpress/wp-content/themes/temat/single.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+
+	<main role="main">
+	<!-- section -->
+	<section>
+
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+			<!-- post thumbnail -->
+			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+				</a>
+			<?php endif; ?>
+			<!-- /post thumbnail -->
+
+			<!-- post title -->
+			<h1>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h1>
+			<!-- /post title -->
+
+			<!-- post details -->
+			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+			<span class="author">Opublikowane przez: <?php the_author_posts_link(); ?></span>
+			<!-- /post details -->
+
+			<?php the_content(); // Dynamic Content ?>
+
+			<?php the_tags( __( 'Tagi: ' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+
+			<p><?php _e( 'Kategorie w: ' ); the_category(', '); // Separated by commas ?></p>
+
+			<p><?php _e( 'Tekst napisany przez  ' ); the_author(); ?></p>
+
+			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+
+		</article>
+		<!-- /article -->
+
+	<?php endwhile; ?>
+
+	<?php else: ?>
+
+		<!-- article -->
+		<article>
+
+			<h1>Przepraszamu, nie mamy co pokazać</h1>
+
+		</article>
+		<!-- /article -->
+
+	<?php endif; ?>
+
+	</section>
+	<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
